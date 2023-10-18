955394b29bb352ae3d24859c5325eb47759f8f59
Brett Clifton <bclifton@adadapted.com>
2023-10-18 12:56:32 -0400

    feat: adding basic ad classes

---
+++ Sources/adadapted-swift-sdk/core/atl/AddToListContentSources.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Brett Clifton on 10/18/23.
+//
+
+import Foundation
+
+class AddToListContentSources {
+    static let IN_APP = "in_app"
+    static let OUT_OF_APP = "out_of_app"
+}
---
+++ Sources/adadapted-swift-sdk/core/payload/Payload.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Brett Clifton on 10/18/23.
+//
+
+import Foundation
