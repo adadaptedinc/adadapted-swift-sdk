ff1727de49332cc80867629b39880491b28a8b69
Brett Clifton <bclifton@adadapted.com>
2024-04-15 16:56:26 -0400

    feat: nullable session listener

--- Sources/adadapted-swift-sdk/core/StartupListener.swift
+++ Sources/adadapted-swift-sdk/core/StartupListener.swift
@@ -6,24 +6,24 @@ import Foundation
 
 class StartupListener: SessionListener {
     
-    var sessionListener: AaSdkSessionListener!
+    var sessionListener: AaSdkSessionListener?
 
-    init(sessionListener: AaSdkSessionListener!) {
+    init(sessionListener: AaSdkSessionListener?) {
         self.sessionListener = sessionListener
     }
     
     func onSessionAvailable(session: Session) {
-        sessionListener.onHasAdsToServe(hasAds: session.hasActiveCampaigns(), availableZoneIds: session.getZonesWithAds())
+        sessionListener?.onHasAdsToServe(hasAds: session.hasActiveCampaigns(), availableZoneIds: session.getZonesWithAds())
         if session.hasActiveCampaigns() && session.getZonesWithAds().isEmpty {
             AALogger.logError(message: "The session has ads to show but none were loaded properly. Is an obfuscation tool obstructing the AdAdapted Library?")
         }
     }
 
     func onAdsAvailable(session: Session) {
-        sessionListener.onHasAdsToServe(hasAds: session.hasActiveCampaigns(), availableZoneIds: session.getZonesWithAds())
+        sessionListener?.onHasAdsToServe(hasAds: session.hasActiveCampaigns(), availableZoneIds: session.getZonesWithAds())
     }
 
     func onSessionInitFailed() {
-        sessionListener.onHasAdsToServe(hasAds: false, availableZoneIds: [])
+        sessionListener?.onHasAdsToServe(hasAds: false, availableZoneIds: [])
     }
 }
